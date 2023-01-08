2228f5f1ed44cacc865806becd8979dfa7f3a4c7
jessanahi <jess.anahi.zamora@gmail.com>
2023-01-08 15:15:59 -0600

    using morgan

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const postBank = require('./postBank');
 app.use(morgan('dev'));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+};
 
 const PORT = 1337;
 
