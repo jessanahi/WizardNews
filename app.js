6d5ea33246330ceb0b22649f5fe2a1b5d4cc840b
safirtrew <safirtrew@gmail.com>
2023-01-08 22:14:17 -0500

    error

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const postBank = require('./postBank');
-app.use(morgan('dev'));
-app.use(express.static('public'));
+const morgan = require("morgan");
+const postBank = require("./postBank");
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const posts = postBank.list();
   const mainhtml = `<!DOCTYPE html>
     <html>
@@ -30,16 +30,19 @@ app.get('/', (req, res) => {
         </small>
       </div>`
         )
-        .join('')}
+        .join("")}
   </div>
 </body>
 </html>`;
   res.send(mainhtml);
 });
 
-app.get('/posts/:id', (req, res) => {
+app.get("/posts/:id", (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
+  if (!post.id) {
+    throw new Error("Not Found");
+  }
   const singlepagehtml = `<!DOCTYPE html>
   <html>
   <head>
@@ -53,11 +56,25 @@ app.get('/posts/:id', (req, res) => {
       ${post.content}
   </body>
   </html>
-`
+`;
   res.send(singlepagehtml);
 });
 
 const PORT = 1337;
+
+app.get("/", (req, res) => {
+  throw new Error("404 NOT FOUND"); // Express will catch this on its own.
+});
+
+// app.get("/", (req, res, next) => {
+//   fs.readFile("/file-does-not-exist", (err, data) => {
+//     if (err) {
+//       next(err); // Pass errors to Express.
+//     } else {
+//       res.send(data);
+//     }
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
